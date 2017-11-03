a939b816752ba21b41f105ef210ee47901cf0301
Sebastian <anse23@hotmail.com>
2017-11-03 12:20:56 +0000

    clean: Removed deleted files

--- cw1/mult_init.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-python cw1_2c.py $1
-python cw1_2c.py $1 -i fii
-python cw1_2c.py $1 -i foi
-python cw1_2c.py $1 -i sei
--- cw1/mult_layers.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-for value in {2..8}
-do
-	python cw1_2b.py $1 -hl $value
-done
