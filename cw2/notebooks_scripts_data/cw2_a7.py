e1ea3421316741ee169361c77cbbb0d2a31a1e3a
Sebastian <anse23@hotmail.com>
2017-11-21 15:54:18 +0000

    feat: Added scripts to run batch normalization experiments [Task A7]

---
+++ cw2/notebooks_scripts_data/cw2_a7.py
@@ -0,0 +1,185 @@
+import argparse
+
+from mlp.layers import AffineLayer, ReluLayer,DropoutLayer, BatchNormalizationLayer
+from mlp.errors import CrossEntropySoftmaxError
+from mlp.models import MultipleLayerModel
+from mlp.initialisers import ConstantInit, GlorotUniformInit
+from mlp.learning_rules import GradientDescentLearningRule
+from collections import OrderedDict
+import numpy as np
+
+from common import load_data, load_data_hog, train_and_save_results, L2Penalty
+
+#########################################################################################
+stats_interval = 1
+seed=10102016
+
+def network_dropout(exp_name, num_epochs):
+    hyper = OrderedDict()
+    hyper["learning_rate"] = 0.001
+    hyper["dropout_prob"] = 0.5
+    hyper["batch_size"] = 50
+    hyper["num_epochs"] = num_epochs
+
+    input_dim, output_dim, hidden_dim = 784, 47, 100
+
+    rng = np.random.RandomState(seed)
+
+    weights_init = GlorotUniformInit(rng=rng, gain=2.**0.5)
+    biases_init = ConstantInit(0.)
+
+    model = MultipleLayerModel([
+        DropoutLayer(rng, hyper["dropout_prob"]),
+        AffineLayer(input_dim, hidden_dim, weights_init, biases_init), 
+        BatchNormalizationLayer(hidden_dim, rng),
+        ReluLayer(),
+
+        DropoutLayer(rng, hyper["dropout_prob"]),
+        AffineLayer(hidden_dim, hidden_dim, weights_init, biases_init), 
+        BatchNormalizationLayer(hidden_dim, rng),
+        ReluLayer(),
+
+        DropoutLayer(rng, hyper["dropout_prob"]),
+        AffineLayer(hidden_dim, output_dim, weights_init, biases_init),
+        BatchNormalizationLayer(output_dim, rng)
+    ])
+
+    error = CrossEntropySoftmaxError()
+    
+    learning_rule = GradientDescentLearningRule(hyper["learning_rate"])
+
+    train_data, valid_data, test_data = load_data(rng, batch_size=hyper["batch_size"])
+
+    train_and_save_results(
+        exp_name + "_network_dropout",
+        model,
+        error,
+        learning_rule,
+        hyper,
+        train_data,
+        valid_data,
+        test_data,
+        stats_interval
+        )
+
+def network_regularization(exp_name, num_epochs):
+    hyper = OrderedDict()
+    hyper["learning_rate"] = 0.001
+    hyper["l2_coeff"] = 1e-2
+    hyper["batch_size"] = 50
+    hyper["num_epochs"] = num_epochs
+
+    input_dim, output_dim, hidden_dim = 784, 47, 100
+
+    rng = np.random.RandomState(seed)
+
+    weights_init = GlorotUniformInit(rng=rng, gain=2.**0.5)
+    biases_init = ConstantInit(0.)
+
+    weights_penalty = L2Penalty(hyper["l2_coeff"])
+    biases_penalty = L2Penalty(hyper["l2_coeff"])
+
+    model = MultipleLayerModel([
+        AffineLayer(input_dim, hidden_dim, weights_init, 
+                    biases_init, weights_penalty=weights_penalty,
+                    biases_penalty=biases_penalty),
+        BatchNormalizationLayer(hidden_dim, rng),
+        ReluLayer(),
+
+        AffineLayer(hidden_dim, hidden_dim, weights_init, 
+                    biases_init, weights_penalty=weights_penalty,
+                    biases_penalty=biases_penalty),
+        BatchNormalizationLayer(hidden_dim, rng),
+        ReluLayer(),
+        
+        AffineLayer(hidden_dim, output_dim, weights_init, 
+                    biases_init, weights_penalty=weights_penalty,
+                    biases_penalty=biases_penalty),
+        BatchNormalizationLayer(output_dim, rng)
+    ])
+
+    error = CrossEntropySoftmaxError()
+    
+    learning_rule = GradientDescentLearningRule(hyper["learning_rate"])
+
+    train_data, valid_data, test_data = load_data(rng, batch_size=hyper["batch_size"])
+
+    train_and_save_results(
+        exp_name + "_network_regularization",
+        model,
+        error,
+        learning_rule,
+        hyper,
+        train_data,
+        valid_data,
+        test_data,
+        stats_interval
+        )
+
+def network_hog(exp_name, num_epochs):
+    hyper = OrderedDict()
+    hyper["learning_rate"] = 0.001
+    hyper["batch_size"] = 50
+    hyper["num_epochs"] = num_epochs
+
+    input_dim, output_dim, hidden_dim = 36, 47, 100
+
+    rng = np.random.RandomState(seed)
+
+    weights_init = GlorotUniformInit(rng=rng, gain=2.**0.5)
+    biases_init = ConstantInit(0.)
+
+    model = MultipleLayerModel([
+        AffineLayer(input_dim, hidden_dim, weights_init, biases_init),
+        BatchNormalizationLayer(hidden_dim, rng),
+        ReluLayer(),
+
+        AffineLayer(hidden_dim, hidden_dim, weights_init, biases_init), 
+        BatchNormalizationLayer(hidden_dim, rng),
+        ReluLayer(),
+        
+        AffineLayer(hidden_dim, output_dim, weights_init, biases_init),
+        BatchNormalizationLayer(output_dim, rng)
+    ])
+
+    error = CrossEntropySoftmaxError()
+    
+    learning_rule = GradientDescentLearningRule(hyper["learning_rate"])
+
+    train_data, valid_data, test_data = load_data_hog(rng, batch_size=hyper["batch_size"])
+
+    train_and_save_results(
+        exp_name + "_network_hog",
+        model,
+        error,
+        learning_rule,
+        hyper,
+        train_data,
+        valid_data,
+        test_data,
+        stats_interval
+        )
+
+def train_networks(exp_name, model_type, num_epochs):
+    if model_type == 0:
+        network_dropout(exp_name, num_epochs)
+    elif model_type == 1:
+        network_regularization(exp_name, num_epochs)
+    elif model_type == 2:
+        network_hog(exp_name, num_epochs)
+    else:
+        print("No valid model")
+
+#########################################################################################
+
+parser = argparse.ArgumentParser(description="Systems with batch normalization for coursework 2")
+parser.add_argument('exp_name', type=str, help="Name of experiment")
+parser.add_argument('model_type', type=int, help="Type of classifier")
+parser.add_argument('-n', dest='num_epochs', type=int, default=100)
+
+args = parser.parse_args()
+exp_name = args.exp_name
+model_type = args.model_type
+num_epochs = args.num_epochs
+
+train_networks(exp_name, model_type, num_epochs)
